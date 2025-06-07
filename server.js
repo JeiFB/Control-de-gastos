1b905d4ec7bf7cb2e9b5980a73375697b825f059
jeifher fuentes <fuentesjeifer@gmail.com>
2025-06-07 16:54:57 -0500

    add login

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Middleware para procesar JSON
 
-function generateToken(email) {
-    const token = jwt.sign({email}, 'secret');
+function generateToken(email, password) {
+    const token = jwt.sign({email, password}, 'secret');
     return token;
 }
 
 app.post('/login', (req, res) => {
     // Verificar si existe email en el body
-    const { email } = req.body;
+    const { email, password } = req.body;
     
-    if (!email) {
+    if (!email || !password) {
         return res.status(400).json({ error: 'Email es requerido' });
     }
 
     try {
-        const token = generateToken(email);
+        const token = generateToken(email, password);
         res.json({ token });
     } catch (error) {
         res.status(500).json({ error: 'Error al generar el token' });
